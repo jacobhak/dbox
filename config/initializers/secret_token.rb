125db76a086c38b65083cfe4db7626db14e2bc98
jacobhak <jacobhakansson@gmail.com>
2013-03-26 14:46:30 +0100

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dbox::Application.config.secret_token = '0e11820693d18612465f8f9564752fa445d9e44091470fe1fabbea68d125f93a1cbdd497dff63ea4fcacde47e2f0972b6b0f0a5530d06c07905855076e58ac15'

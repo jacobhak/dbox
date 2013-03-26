552801246b442f926af2cb81c4200e106d7e3add
Simon Strom <simon.strom@gmail.com>
2013-03-26 22:09:08 +0100

    test status code 200 for static#index

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe StaticController do
-
+  describe "GET #index" do
+    it "responds with 200" do
+      get :index
+      expect(response).to be_success
+      expect(response.code).to eq("200")
+    end
+  end
 end
